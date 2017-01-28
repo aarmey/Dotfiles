598034c9cecac6e89ac7c3896ad3be86c53605d7
Aaron Meyer <a@ameyer.me>
2017-01-28 11:36:18 -0500

    Add count command for brew dep.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,5 @@ set fish_greeting ""
 set -gx PATH ~/.bin $PATH
 
 alias selfcontrol "sudo /Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl (id -u (whoami)) --install"
+
+alias brewdepcount "brewdeps.sh | tr ' ' '\n' | grep -v '^\s*\$/' | sort | uniq -c | sort -bnr"
