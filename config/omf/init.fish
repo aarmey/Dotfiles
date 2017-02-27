e35ac2575ff631c3ecd3e4e7dc63607c142665aa
Aaron Meyer <a@ameyer.me>
2017-02-27 09:08:43 -0500

    Turn ARD on and off.

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,7 +1,7 @@
 set -gx PATH ~/.bin $PATH
 set -gx PATH ~/node_modules/.bin $PATH
 
-
 alias selfcontrol "sudo /Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl (id -u (whoami)) --install"
-
+alias sharingOn "sudo  /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -configure -allowAccessFor -allUsers -configure -restart -agent -privs -all"
+alias sharingOff "sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off"
 alias brewdepcount "brewdeps.sh | tr ' ' '\n' | grep -v '^\s*\$/' | sort | uniq -c | sort -bnr"
