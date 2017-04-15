4816d161f54c499ed01e0d16dff1afa5019f1c01
Aaron Meyer <a@ameyer.me>
2017-04-15 17:28:38 -0400

    More changes from cleanup.

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,5 +1,4 @@
 set -gx PATH ~/.bin $PATH
-set -gx PATH ~/node_modules/.bin $PATH
 
 alias selfcontrol "sudo /Applications/SelfControl.app/Contents/MacOS/org.eyebeam.SelfControl (id -u (whoami)) --install"
 alias sharingOn "sudo  /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -configure -allowAccessFor -allUsers -configure -restart -agent -privs -all"
