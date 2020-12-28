82ae04d1f224957517b045c47e383d80e28ff9d1
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-12-28 07:46:50 -0800

    Update osxdefaults.sh

--- bin/osxdefaults.sh
+++ bin/osxdefaults.sh
@@ -1,8 +1,3 @@
-
-
-# Enable dark mode
-sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
-
 # Show Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool true
 
